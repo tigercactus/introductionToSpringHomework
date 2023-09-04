3ec97c9bf23268cb4347cd2dcdbbe855573af81c
tigercactus <tigercactus@yandex.ru>
2023-09-04 22:13:39 +0300

    мда что коммитить то

---
+++ src/main/java/pro/sky/introductionToSpringHomework/IntroductionToSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.introductionToSpringHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringHomeworkApplication.class, args);
+	}
+
+}
